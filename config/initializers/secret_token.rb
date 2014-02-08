b4b3270284764076303b949fdfd435c84473623d
Ryan <hiphubryan@gmail.com>
2014-02-08 16:22:09 +0800

    new rails project named groupme to implement rails101

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Groupme::Application.config.secret_key_base = 'f6256500da67d35f5b90ccbfbb6b83062c715c88c54d287a7fa0e533c2ee7c4e0e7e517eb5171328fe8f9407e3243030ea773dae864c0909e95cb937d8d19246'
